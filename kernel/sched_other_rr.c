a847897cb6b1975804b68fc1e3706317674a7fe1
Keeler <krussell@eecs678-kvm.eecs.ku.edu>
2013-04-04 21:39:53 -0500

    First crack at sched_other_rr.c

--- kernel/sched_other_rr.c
+++ kernel/sched_other_rr.c
@@ -31,7 +31,8 @@ static void update_curr_other_rr(struct rq *rq)
  */
 static void enqueue_task_other_rr(struct rq *rq, struct task_struct *p, int wakeup, bool b)
 {
-	// not yet implemented
+	list_add_tail( &p->other_rr_run_list, &rq->other_rr.queue );
+	rq->other_rr.nr_running++;
 }
 
 static void dequeue_task_other_rr(struct rq *rq, struct task_struct *p, int sleep)
@@ -39,7 +40,8 @@ static void dequeue_task_other_rr(struct rq *rq, struct task_struct *p, int slee
 	// first update the task's runtime statistics
 	update_curr_other_rr(rq);
 
-	// not yet implemented
+	list_del( &rq->curr.other_rr_run_list );
+	p->other_rr.nr_running--;
 }
 
 /*
@@ -57,7 +59,7 @@ static void requeue_task_other_rr(struct rq *rq, struct task_struct *p)
 static void
 yield_task_other_rr(struct rq *rq)
 {
-	// not yet implemented
+	requeue_task_other_rr( rq, &rq->curr );
 }
 
 /*
@@ -77,16 +79,20 @@ static struct task_struct *pick_next_task_other_rr(struct rq *rq)
 	struct list_head *queue;
 	struct other_rr_rq *other_rr_rq;
 
-	// not yet implemented
+	if( list_empty( rq->other_rr.queue ) )
+	{
+		return NULL;
+	}
+
+	next = rq->curr;
 
 	/* after selecting a task, we need to set a timer to maintain correct
 	 * runtime statistics. You can uncomment this line after you have
 	 * written the code to select the appropriate task.
 	 */
-	//next->se.exec_start = rq->clock;
-	
-	/* you need to return the selected task here */
-	return NULL;
+	next->se.exec_start = rq->clock;
+
+	return next;
 }
 
 static void put_prev_task_other_rr(struct rq *rq, struct task_struct *p)
@@ -179,7 +185,22 @@ static void task_tick_other_rr(struct rq *rq, struct task_struct *p,int queued)
 	// first update the task's runtime statistics
 	update_curr_other_rr(rq);
 
-	// not yet implemented
+	// Want to do first come, first served if time quantum is 0.
+	// That means doing nothing in this function.
+	if( other_rr_time_slice == 0 )
+	{
+		return;
+	}
+	else
+	{
+		// Decrement the task's time slice counter, and if it reaches 0,
+		// reschedule it.
+		if( p->task_time_slice-- == 0 )
+		{
+			set_tsk_need_resched( p );
+		}
+
+	}
 }
 
 /*
